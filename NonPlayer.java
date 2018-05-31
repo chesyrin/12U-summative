a3758bd1cad19f9828a5aad1bbf010ded28170d2
chesyrin <chesyrin@users.noreply.github.com>
2018-05-31 11:04:43 -0400

    Add files via upload

---
+++ NonPlayer.java
@@ -0,0 +1,12 @@
+/* NonPlayer.java
+ * Julia Zhao and Tasha Xiao
+ * NPC class for the 12U final project
+ * May 31 2018
+ */
+
+public class NonPlayer {
+  
+  public void talk(){
+  }
+  
+}
---
+++ Player.java
@@ -0,0 +1,12 @@
+/* Player.java
+ * Julia Zhao and Tasha Xiao
+ * Player(user) class for the 12U final project
+ * May 31 2018
+ */
+
+public class Player extends Character{
+  
+  public void talk(){
+    //talk to NPC
+  }
+}
