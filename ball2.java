5ca92f2426d3738281be8a495ced1fd4619812e2
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-06-12 11:01:55 -0400

    Update ball2.java

--- ball2.java
+++ ball2.java
@@ -1,14 +1,17 @@
 /*
  * Main gui 
+ * change boundaries
  */
+import java.io.*;
+import java.util.Scanner;
 import javax.swing.*;  
-import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.concurrent.TimeUnit;
 
-public class ball2{
-  Object tako = new Object();
+public class ball2 {
+  
+  private SimpleLinkedList<Object> interactObj = initial();
   JFrame frame = new JFrame("Key Listener");
   JPanel p = new JPanel();
   final JLabel frontAni[] = new JLabel[3];
@@ -17,8 +20,9 @@ public class ball2{
   final JLabel leftAni[] = new JLabel[3];
   
   int x = 8, y = 100, velx =0, vely =0, count = 0;
-  public ball2(){
-    
+  private SimpleLinkedList<Item> itemList;
+  public ball2(SimpleLinkedList<Item> itemsList){
+    this.itemList = itemsList;
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(615,425);
     frame.setVisible(true);
@@ -73,16 +77,14 @@ public class ball2{
     frontAni[0].setVisible(true);//this sprite is initially visible
     
     
-    //set textfield for keyboard input
-    final JTextField textField = new JTextField("in");
+    //set keyboard input
     KeyListener listener = new KeyListener() {
       
       public void keyPressed(KeyEvent event) {
         //set velocity x & velocity y
         keyboardLocation(event.getKeyCode());
-        //clear textField (in case a letter on keyboard is pressed)
-        textField.setText("");
-        
+
+//        System.out.println(x + " " + y);
         //sets general bounds
         if(x < 8){//left boundary
           x = 8;  
@@ -92,38 +94,43 @@ public class ball2{
           x = 550;  
         }
         
-        if(y < 100){//upper bounds
-          y = 100;  
+        if(y < 80){//upper bounds
+          y = 80;  
         }
         
         if(y > 320){//lower bounds
           y = 320;  
         }
-        //tako boundary test
-        //currently left, trying to move right
-        //if the character sprite is inside the object image
-        if (x>tako.getX() && x<tako.getX() + 2*tako.getSx()  && //x-bounds
-            y > tako.getY() && y < (tako.getY() + tako.getSy())){//y-bounds
-          //teleport the player sprite to the appropriate side of the object image
-          //if it's on the left side of the image
-          if (x - 1 == tako.getX()){
-            x = tako.getX();
-          }
-          //if it's on the right side of the image
-          else if (x + 1 == tako.getX() + 2*tako.getSx()) {
-            x = tako.getX() + 2*tako.getSx();
-          }
-          //if it's on the top of the image
-          else if (y - 1 == tako.getY()){
-            y = tako.getY();
-          }
-          //else on bottom of image
-          else {
-            y = tako.getY() + tako.getSy();
+        boolean ifMoved = false;
+        for (int i = 0; i<interactObj.size(); i ++){
+          Object temp = interactObj.get(i);
+          //temp boundary test
+          //currently left, trying to move right
+          //if the character sprite is inside the object image
+          if (x>temp.getX() && x<temp.getSx() && //x-bounds
+              y > temp.getY() && y < + temp.getSy()){//y-bounds
+            ifMoved = true;
+            //teleport the player sprite to the appropriate side of the object image
+            //if it's on the left side of the image
+            if (x - 3 < temp.getX()){
+              x = temp.getX();
+            }
+            //if it's on the right side of the image
+            else if (x + 3 > temp.getSx()) {
+              x = temp.getSx();
+            }
+            //if it's on the top of the image
+            else if (y - 3 < temp.getY()){
+              y = temp.getY();
+            }
+            //else on bottom of image
+            else {
+              y = temp.getSy();
+            }
           }
         }
         //set x and  y
-        else {
+        if (ifMoved == false) {
           x += velx;
           y += vely;
         }
@@ -206,105 +213,8 @@ public class ball2{
         vely=0;
       }
       
-      //readItems();
       public void keyTyped(KeyEvent event) {
-        System.out.println (event.getKeyChar());
-        //for action buttons
-        //x for interacting with objects
-        if (event.getKeyChar()=='x'){
-          boolean isItem = Main.checkItem(x,y);
-          // if (isItem==true){
-          System.out.println ("JJJJ");
-          //3 possible item locations
-          boolean alreadyInside = false;
-          //if (x>10 && x<100 && y>10 && y<100){ //example coordinates, please put in the correct ones later
-          //try to find the item in the inventory to prevent duplicates
-          for (int i=0; i<Main.getInventory().size(); i++){
-            if (Main.getInventory().get(i).getName().equals("Envelope")){
-              System.out.println ("hi");
-              alreadyInside=true;
-            }
-          }
-          //}
-          System.out.println (alreadyInside);
-          if (alreadyInside==false){
-            System.out.println (Main.getItems().get(0).getName());
-            //add envelope into inventory
-            Main.getInventory().add(Main.getItems().get(0));
-          }
-//            if (alreadyInside==false){
-//              //add envelope into inventory
-//              inventory.add(items.get(0));
-//            }
-          // }
-//            else if (){
-//              for (i=0; i<inventory.size(); i++){
-//                if (Main.inventory.get(i).getName().equals("Fork")){
-//                  alreadyInside=true;
-//                }
-//              }
-//              
-//              if (alreadyInside==false){
-//                //add fork into inventory
-//                Main.inventory.add(Main.items.(get(1)));
-//              }
-//            }
-//            else if (){
-//              for (i=0; i<inventory.size(); i++){
-//                if (Main.inventory.get(i).getName().equals("Mirror")){
-//                  alreadyInside=true;
-//                }
-//              }
-          
-//              if (alreadyInside==false){
-//                //add envelope into inventory
-//                Main.inventory.add(Main.items.get(2));
-//              }
-//        }
-        }
-        //press c for inventory
-        else if (event.getKeyChar()=='c'){
-          JFrame frame = new JFrame("Inventory");
-          JPanel panel = new JPanel();
-          SimpleLinkedList <JLabel> itemName = new  SimpleLinkedList <JLabel>();
-          SimpleLinkedList <JTextArea> itemDesc = new  SimpleLinkedList <JTextArea>();
-          Font boldFont = new Font("Courier", Font.BOLD,12);
-          
-          for (int i=0; i<Main.getInventory().size(); i++){
-            itemName.add(new JLabel(Main.getInventory().get(i).getName()));
-            itemName.get(i).setFont(boldFont);
-            
-            itemDesc.add(new JTextArea(Main.getInventory().get(i).getDesc()));
-            itemDesc.get(i).setLineWrap(true); //so that the text doesn't go beyond the frame
-            itemDesc.get(i).setWrapStyleWord(true);
-            
-            panel.add(itemName.get(i)); //add items to panel
-            panel.add(itemDesc.get(i));
-          }
-          frame.add(panel);
-          
-          frame.setSize(400, 800);
-          frame.setVisible(true);
-        }
-        //press z to talk to npc
-        else if (event.getKeyChar()=='z'){
-         // if (x>10 && x<100 && y>40 && y<300){ //if user is standing in front of NPC
-            new VisualNovel (Main.getPuzzles(), Main.getItems());
-          //}
-        }
-        
-        //press v to open the door
-        else if (event.getKeyChar()=='v'){
-          if (x>200 && x<300 && y>10 && y<200 && Main.getInventory().get(0).getName().equals("Key")){ //use door's coordinates
-            //triger the ending dialogue
-            new VisualNovel ("end.txt");
-          }
-        }
-        System.out.println ("\nItem added into inventory!");
-        for (int i=0; i<Main.getInventory().size(); i++){
-          System.out.println (Main.getInventory().get(i).getName());
-        }
-      };
+      }
       
       private void keyboardLocation(int keybrd) {
         if (keybrd == KeyEvent.VK_DOWN){
@@ -322,21 +232,22 @@ public class ball2{
         if (keybrd == KeyEvent.VK_RIGHT){
           vely = 0;
           velx = 2;
+          
+        }
+        if (keybrd == KeyEvent.VK_X){
+          new ItemPopUp();
+          
         }
       }
       
-    };//end of key listner
+      
+    };//end of key listener
     p.setLayout(null);
-    //add tako sprite test
-    JLabel takoTemp = tako.getSprite();
-    takoTemp.setBounds(tako.getX() + tako.getSx(), tako.getY() +  tako.getSy(), 
-                       tako.getSx(), tako.getSy());//location (x,y), size(x,y)
-    p.add(takoTemp);
     //add the keyboard listener
     frame.addKeyListener(listener);
     
     //add bg image
-    ImageIcon bgImage = new ImageIcon("IMAGE.jpg");
+    ImageIcon bgImage = new ImageIcon("IMAGE1.jpg");
     JLabel bg = new JLabel(bgImage);
     bg.setBounds(0,0,600,386);
     p.add(bg);
@@ -346,7 +257,7 @@ public class ball2{
     
     frame.setVisible(true);
   }
-  //sets all the sprites visiblity to false
+//sets all the sprites visiblity to false
   public void reset(){
     for (int i = 0; i<3; i++){
       frontAni[i].setVisible(false);
@@ -355,10 +266,20 @@ public class ball2{
       rightAni[i].setVisible(false);
     }
   }
-  public static void main(String args[]) {
-    //readItems();
-    new ball2();
-    
+  //initializes the objects
+  public SimpleLinkedList<Object> initial(){
+    SimpleLinkedList<Object> a = new SimpleLinkedList<Object>();
+    try {
+      Scanner fileInput = new Scanner (new File ("objectList.txt"));
+      while (fileInput.hasNextLine()){
+        Scanner stringInput = new Scanner(fileInput.nextLine());
+        Object temp = new Object(stringInput.next(), stringInput.next(), 
+                                 stringInput.next(), stringInput.next(),
+                                 stringInput.next());
+        a.add(temp);
+      }//end of while
+      fileInput.close();}
+    catch(Exception e){System.out.println("error");}
+    return a;
   }
-  
 }
